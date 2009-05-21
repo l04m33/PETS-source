635a0568caa5bd8bc6956d8469f68f44e6b04b28
Kay Zheng <l04m33@gmail.com>
2009-05-21 11:20:09 +0800

    added a connection pool impl. and some empty modules, plus the 'LocalFileDA' class

---
+++ db/cpool.py
@@ -0,0 +1,68 @@
+import sqlite3
+import threading
+
+class ConnectionPool:
+
+  def __init__(self, db_path, n):
+    self.pool = []
+    self.plock = threading.Lock()
+    self.pool_size = n
+    self.db_path = db_path
+    for i in range(n):
+      conn = sqlite3.connect(db_path)
+      self.pool.append(conn)
+
+  def get_connection(self):
+    self.plock.acquire()
+    if len(self.pool) > 0:
+      conn = self.pool.pop(0)
+      self.plock.release()
+      return conn
+    self.plock.release()
+    conn = sqlite3.connect(self.db_path)
+    return conn
+
+  def put_connection(self, conn):
+    self.plock.acquire()
+    if len(self.pool) < self.pool_size:
+      self.pool.append(conn)
+      self.plock.release()
+      return
+    self.plock.release()
+    conn.close()
+
+  # this needs more synchronization
+  # given-away connections may still be in action
+  def release(self):
+    self.plock.acquire()
+    for c in self.pool:
+      c.close()
+    self.pool = []
+    self.plock.release()
+
+
+__def_cpool = None
+
+# this function should be run before any attempt to access the pool
+def cpool_init(db_path, n):
+  global __def_cpool
+  if __def_cpool is not None:
+    __def_cpool.release()
+  __def_cpool = ConnectionPool(db_path, n)
+
+def get_cpool():
+  global __def_cpool
+  return __def_cpool
+
+def cpool_release():
+  global __def_cpool
+  __def_cpool.release()
+  __def_cpool = None
+
+
+if __name__ == '__main__':
+  cpool_init("/home/l_amee/client_share/P.E.T.S/tmp/pets_def_db.sqlite3", 4)
+  conn = get_cpool().get_connection()
+  print conn
+  get_cpool().put_connection(conn)
+  get_cpool().release()
--- db/da.py
+++ db/da.py
@@ -1,15 +1,70 @@
-from support import *
+import sqlite3
+import support as supp
+import cpool
+
+class NoActiveConnectionError(Exception):
+  pass
 
 class LocalFileDA:
-  def begin_transaction():
-    pass
 
-  def end_transaction();
-    pass
+  def __init__(self):
+    self.__conn = None
+    self.cpool = cpool.get_cpool()
 
-  def save_row(row):
-    pass
+  # sqlite locks the db if there's a dirty session
+  # no locking should be done here
+  def begin_transaction(self):
+    # drop all the changes if there's already an active session
+    if self.__conn is not None:
+      self.__conn.rollback()
+    self.__conn = self.cpool.get_connection()
 
-  def save_seg_row(seg_row):
-    pass
+  def end_transaction(self):
+    self.__check_conn()
+    self.__conn.commit()
+    self.cpool.put_connection(self.__conn)
+    self.__conn = None
 
+  def rollback(self):
+    self.__check_conn()
+    self.__conn.rollback()
+
+  def save_row(self, row):
+    self.__check_conn()
+    self.__conn.execute(supp.get_sql('local_files.insert') % row)
+
+  def del_by_hash(self, hash):
+    self.__check_conn()
+    self.__conn.execute(supp.get_sql('local_files.delete_by_pk') % (hash,))
+
+  def save_seg_row(self, seg_row):
+    self.__check_conn()
+    self.__conn.execute(supp.get_sql('local_segs.insert') % seg_row)
+
+  def del_seg_by_hash(self, seg_hash):
+    self.__check_conn()
+    self.__conn.execute(supp.get_sql('local_segs.delete_by_pk') % (seg_hash,))
+
+  def __check_conn(self):
+    if self.__conn is None:
+      raise NoActiveConnectionError("maybe you should 'begin_transaction()' before calling this")
+
+
+
+if __name__ == '__main__':
+  cpool.cpool_init('/home/l_amee/client_share/P.E.T.S/tmp/pets_def_db.sqlite3', 5)
+
+  # testing for the LocalFileDA
+  lfda = LocalFileDA()
+
+  lfda.begin_transaction()
+  lfda.save_row(('file_hash', 'full_name', 'name', 4096, '2009', 8192))
+  lfda.save_seg_row(('seg_hash', 0, 'file_hash', 'true'))
+  lfda.save_seg_row(('seg_hash1', 4096, 'file_hash', 'false'))
+  lfda.end_transaction()
+
+  lfda.begin_transaction()
+  lfda.del_seg_by_hash('seg_hash')
+  lfda.del_seg_by_hash('seg_hash1')
+  lfda.del_by_hash('file_hash')
+  lfda.end_transaction()
